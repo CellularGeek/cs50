8156fc1aac04038f3a798ea762e37cdf58511de6
CellularGeek <imperialduks@gmail.com>
2014-04-25 20:03:26 -0400

    Update caesar.c

--- caesar.c
+++ caesar.c
@@ -1,5 +1,6 @@
 /* Pset2
  * Caesar.c
+ * This program will encrypt user input using using caesar cipher.
  * Written by Mohamed Dukuray
  * CS50 20/04/2014
  */
@@ -13,27 +14,40 @@
  //to get Key from the user
  int main( int argc, string argv[1])
  {
+    if (argc != 2)
+    {
+        printf("Usage: ./caesar key");
+        printf("\n");
+        return 1;
+    }
     //cast key "string" to "int"
     int key = atoi (argv[1]);
-    argc = 2;
+    
     
     //get string from user
-    string uString;
-    do
-    {
-        uString = GetString();
-    }
-    while ( uString != NULL);
-    
-    for (int i = 0; i < argc; i++)
-    
-    for (int i = 0, n = strlen(uString); i < n; i++)
-    {
-        if (isupper(uString[i]) || islower(uString[i]))
-        {
-            printf("%c", (uString[i]+key)%26); 
-        } 
-        else
+    string userStr = GetString();
+     
+     //iterates over the chars* of string   
+     for (int i = 0, n = strlen(userStr); i < n; i++)
+            {
+                
+                // print out cipher
+                if (isupper(userStr[i]))
+                {
+                    printf("%c", (userStr[i] - 'A'+ key)%26 +'A'); 
+                } 
+                else if (islower(userStr[i]))
+                {
+                    printf("%c", (userStr[i] -'a' + key)%26 + 'a'); 
+                }
+                else
+                {
+                   printf("%c", userStr[i]); 
+                }
+             }   
+     printf("\n");
+     return 0;              
+ }
         {
            printf("%c", (uString[i]+key)%26); 
         }
